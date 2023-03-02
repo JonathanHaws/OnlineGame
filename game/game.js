f148cd3f7acc33b899ed698e229358e6b96c21e2
JonathanHaws <jonathanmhaws1@gmail.com>
2023-03-02 14:36:00 -0700

    server side logic

--- game/game.js
+++ game/game.js
@@ -1,22 +1,21 @@
-let players = {};
-let player;
+
+let game = {};
 function setup(){
   createCanvas(1280,720);
   let name = prompt("Please enter your name:");
-  console.log(name);
-  player = new Player(width/2, height/2, name);
-  socket = io.connect();// connect to the server
-  setInterval(() => { socket.emit('update',{x:player.x, y:player.y, name:player.name, health:player.health, sprite:player.sprite})},1000/60); 
-  socket.on('update', function(data) {players = data;});
+  socket = io.connect('',{query:"name="+name});;// connect to the server
+  setInterval(()=>{socket.emit('input',{ 
+    x:((keyIsDown(68)||keyIsDown(RIGHT_ARROW))-(keyIsDown(65)||keyIsDown(LEFT_ARROW))),
+    y:(keyIsDown(87)||keyIsDown(UP_ARROW)||keyIsDown(32)-(keyIsDown(83)||keyIsDown(DOWN_ARROW)))})},
+    1000/60); //
+  socket.on('update',function(data){game=data;});
 }
   
-function draw(){ //console.log(players); //list of all players on server
+function draw(){ //console.log(game); //all game data from server
   background(220);
   fill(100);
   rect(0, height/2, width, height/2);
-
-  for (let p in players){ 
-    let player = players[p];
+  for (let i in game.players){let player=game.players[i];
     fill('#3b3b3b80');
     textSize(20); textAlign(CENTER);
     text(player.name, player.x, player.y-20);
@@ -26,47 +25,7 @@ function draw(){ //console.log(players); //list of all players on server
     } else {
       circle(player.x, player.y, 20);
     }
-    rect(player.x - 25, player.y - 40, (player.health/100)*50, 5);
+    rect(player.x - 25, player.y - 45, (player.health/100)*50, 5);
   }
-  player.update();
 }
   
-class Player{
-  constructor(x, y, name) {
-    this.name = name;
-    this.sprite = 'idle';
-    this.health = 100;
-    this.x = x; this.y = y;
-    this.xv = 0; this.yv = 0;
-    this.speed = 4;
-    this.falling = 0;
-    this.size = 20;
-  }
-  update(){
-    let xinput = ((keyIsDown(68)||keyIsDown(RIGHT_ARROW))-(keyIsDown(65)||keyIsDown(LEFT_ARROW)))
-    let yinput = (keyIsDown(87)||keyIsDown(UP_ARROW)||keyIsDown(32)-(keyIsDown(83)||keyIsDown(DOWN_ARROW)))
-    this.yv += 1; this.falling += 1;// Apply gravity
-    this.xv = xinput* this.speed; // Move the player left or right
-    if ((yinput==1) && this.falling<3){ this.yv = -15} 
-    this.x += this.xv; this.y += this.yv; // Move the player
-    if(this.y > (height/2 - this.size/2)){ // collision bottom of screen
-      this.falling = 0; 
-      this.y = (height/2 - this.size/2) 
-    }
-    if(this.x<0){this.x+= width} if(this.x>width){this.x-= width}; //wrap 
-    if(yinput ==-1){ this.attack()}      
-
-  }
-  async attack(){
-    this.sprite='attack'
-    await new Promise(resolve => setTimeout(resolve, 200));
-    this.sprite='idle'
-  }
-
-
-  reset(){
-    this.x = this.spawnx; this.y = this.spawny;
-    this.velX = 0; this.velY = 0;
-  }
-}
-
--- server.js
+++ server.js
@@ -1,40 +1,56 @@
 
 const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const io = require('socket.io')(server);
-const port = process.env.PORT || 3001;
-
+const app     = express();
+const http    = require('http');
+const server  = http.createServer(app);
+const io      = require('socket.io')(server);
+const port    = process.env.PORT || 3001;
 app.use(express.static('game'));
 
-// var game = {players:{},objects:{}} // holds all the data that needs to be sync between clients
-// function tick(){ io.emit('update', game);} //send every client the game data
-// setInterval(tick,1000/30); //tickrate is 30
+class Player{
+  constructor(x,y,name){
+    this.input = {x:0,y:0};
+    this.name = name;
+    this.sprite = 'idle';
+    this.health = 100;
+    this.x = x; this.y = y;
+    this.xv = 0; this.yv = 0;
+    this.speed = 4;
+    this.falling = 0;
+    this.size = 20;
+  }
+  update(){ //console.log(xinput);
+    this.yv += 1; this.falling += 1;// Apply gravity
+    this.xv = this.input.x* this.speed; // Move the player left or right
+    if ((this.input.y==1) && this.falling<3){ this.yv = -15} 
+    this.x += this.xv; this.y += this.yv; // Move the player
+    if(this.y>(360-this.size/2)){ // collision bottom of screen
+      this.falling = 0; 
+      this.y=(360-this.size/2) 
+    }
+    if(this.x<0){this.x+=1280}; if(this.x>1280){this.x-=1280}; //wrap 
+    if(this.input.y ==-1){ this.attack()}      
+  }
+  async attack(){
+    this.sprite='attack'
+    await new Promise(resolve => setTimeout(resolve, 200));
+    this.sprite='idle'
+  }
+  death(){ this.velX = 0; this.velY = 0;}
+}
 
-var players = {};
-io.on('connection',(socket)=>{                      
-  if (Object.keys(players).length >= 4) { 
-    console.log('Server Full');
-    socket.emit('Server Full (Max 4 Players)');
-    socket.disconnect();
-    return;
-  } else { console.log(`New player connected: ${socket.id}`);}
-
-  players[socket.id] = {x:0, y:0, name:"", health:100, sprite:"idle" };
-  socket.on('update',(data)=>{
-    players[socket.id].x = data.x;
-    players[socket.id].y = data.y;
-    players[socket.id].name = data.name;
-    players[socket.id].health = data.health;
-    players[socket.id].sprite = data.sprite;
-    io.emit('update', players);
-  });
-  socket.on('disconnect',()=>{ console.log(`Player disconnected: ${socket.id}`);
-    delete players[socket.id];
-    io.emit('update', players);
-  });
+var game = {players:{},objects:{}} // holds all the data that needs to be sync between clients
+function tick(){ 
+  for (let i in game.players){ game.players[i].update();}
+  io.emit('update', game);//send every client the game data
+} 
+setInterval(tick,1000/60); //tickrate
+io.on('connection',(socket)=>{ //handle player joining server                
+  if (Object.keys(game.players).length >= 4){ socket.disconnect(); return;} //server full
+  console.log(`New Player ${socket.id}`);
+  game.players[socket.id] = new Player(0,0,socket.handshake.query.name); //console.log(game);
+  socket.on('input',(input)=>{ game.players[socket.id].input = input})
+  socket.on('disconnect',()=>{ console.log(`Player Left ${socket.id}`); delete game.players[socket.id];});
 })
 
-   
-server.listen(port,()=>{ console.log(`Server is running on port ${port}.`)});
+server.listen(port,()=>{ console.log(`Server listening ${port}.`)});
